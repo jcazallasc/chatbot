2eee25ec7adca9b3316ae07f7e062ceed6229dd7
Javier Cazallas <cazallas@MacBook-Pro-de-Javier.local>
2020-08-10 09:43:55 +0200

    Adding models

--- app/chatbot/admin.py
+++ app/chatbot/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from chatbot.models import CustomUser, Notification
+
+admin.site.register(CustomUser)
+admin.site.register(Notification)
--- app/chatbot/models.py
+++ app/chatbot/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import JSONField
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=255)
+
+    REQUIRED_FIELDS = ['email', 'phone']
+
+    def __str__(self):
+        return self.username
+
+
+class Notification(models.Model):
+
+    EMAIL = 'Email'
+    VIA_CHOICES = [
+        (EMAIL, _('Email')),
+    ]
+
+    user = models.ForeignKey(
+        CustomUser,
+        related_name='notifications',
+        on_delete=models.CASCADE,
+    )
+    via = models.CharField(
+        max_length=140,
+        choices=VIA_CHOICES,
+        default=EMAIL,
+    )
+    extra_data = JSONField(blank=True, null=True)
+    sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return 'Notification to {} via {}'.format(self.user.username, self.via)
